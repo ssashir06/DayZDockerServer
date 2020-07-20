0c6b037c5e2fab956192d4a6351745be6fde856d
Hiroshi TAKEDA <takejaponpon+git@gmail.com>
2020-07-20 12:46:47 +0000

    Prevent to some errors (WIP)

--- DayZDockerServer/run.sh
+++ DayZDockerServer/run.sh
@@ -16,6 +16,13 @@ fi
 rm -rf ./mpmissions
 ln -s /DayZMission/mpmissions ./mpmissions
 
+# run dbus
+service dbus start
+
+# launch X server
+Xvfb :99 -screen 0 640x480x24:32 &
+export DISPLAY=:99
+
 # start server
 wine64 DayZServer_x64.exe -config=Z:\\DayZConfig\\serverDZ.cfg -profiles=Z:\\DayZProfiles -port=2302 -dologs -adminlog -netlog -freezecheck;
 echo "End DayZ Docker Server"
